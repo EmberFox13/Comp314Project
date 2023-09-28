0cc41a6578491763eb649d9bfe49b657a17a7053
EmberFox13 <133859310+EmberFox13@users.noreply.github.com>
2023-09-28 13:30:26 +0200

    Fixed the logic and code for TokenizationFunctionality
    Signed-off-by: EmberFox13 <133859310+EmberFox13@users.noreply.github.com>

--- src/functionality/TokenizationFunctionality.java
+++ src/functionality/TokenizationFunctionality.java
@@ -1,101 +1,143 @@
 package functionality;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 public class TokenizationFunctionality {
 
     enum State {
-        START, IDENTIFIER, OPERATOR, STRING_LITERAL, WHITESPACE
+        START,
+        ALPHABET,
+        DIGIT,
+        OPERATOR,
+        WHITESPACE,
+        SPECIAL_CHAR
 
     }
 
     public static List<String> tokenizeString(String input) {
         List<String> tokens = new ArrayList<>();
-        State currentState = State.START;
         StringBuilder currentToken = new StringBuilder();
+        State currentState = State.START;
+        Queue<Character> charQueue = new LinkedList<>();
 
         for (char c : input.toCharArray()) {
-            switch (currentState) {
-                case START:
-                    if (Character.isLetter(c) || c == '.') {
-                        currentState = State.IDENTIFIER;
-                        currentToken.append(c);
-                    } else if (isOperator(c)) {
-                        currentState = State.OPERATOR;
-                        currentToken.append(c);
-                    } else if (c == '"') {
-                        currentState = State.STRING_LITERAL;
-                        currentToken.append(c);
-                    } else if (Character.isWhitespace(c)) {
-                        currentState = State.WHITESPACE;
-                    }
-                    break;
-
-                case IDENTIFIER:
-                    if (Character.isLetterOrDigit(c) || c == '.') {
-                        currentToken.append(c);
-                    } else {
-                        tokens.add(currentToken.toString());
-                        currentToken.setLength(0);
-                        currentState = State.START;
-                    }
-                    break;
-
-                case OPERATOR:
-                    if (isOperator(c)) {
-                        currentToken.append(c);
-                    } else {
-                        tokens.add(currentToken.toString());
-                        currentToken.setLength(0);
-                        currentState = State.START;
-                    }
-                    break;
-
-                case STRING_LITERAL:
-                    currentToken.append(c);
-                    if (c == '"') {
-                        tokens.add(currentToken.toString());
-                        currentToken.setLength(0);
-                        currentState = State.START;
-                    }
-                    break;
-
-                case WHITESPACE:
-                    if (!Character.isWhitespace(c)) {
-                        if (Character.isLetter(c) || c == '.') {
-                            currentState = State.IDENTIFIER;
-                            currentToken.append(c);
-                        } else if (isOperator(c)) {
-                            currentState = State.OPERATOR;
-                            currentToken.append(c);
-                        } else if (c == '"') {
-                            currentState = State.STRING_LITERAL;
-                            currentToken.append(c);
-                        }
-                    }
-                    break;
-            }
+            charQueue.offer(c);
         }
 
-        // Add the last token, if any
+        while (!charQueue.isEmpty()) {
+            char c = charQueue.peek();
+
+            switch (currentState) {
+                case START -> {
+                    if (Character.isLetter(c)) {
+                        currentToken.setLength(0);
+                        currentToken.append(c);
+                        charQueue.poll();
+                        currentState = State.ALPHABET;
+
+                    } else if (Character.isDigit(c)) {
+                        currentToken.setLength(0);
+                        currentToken.append(c);
+                        charQueue.poll();
+                        currentState = State.DIGIT;
+
+                    } else if (isOperator(c)) {
+                        currentToken.setLength(0);
+                        currentToken.append(c);
+                        charQueue.poll();
+                        currentState = State.OPERATOR;
+
+                    } else if (Character.isWhitespace(c)) {
+                        currentToken.setLength(0);
+                        currentToken.append(c);
+                        charQueue.poll();
+                        currentState = State.WHITESPACE;
+
+                    } else {
+                        currentToken.setLength(0);
+                        currentToken.append(c);
+                        charQueue.poll();
+                        currentState = State.SPECIAL_CHAR;
+
+                    }
+                } case ALPHABET -> {
+                    if (Character.isLetter(c) || Character.isDigit(c)) {
+                        currentToken.append(c);
+                        charQueue.poll();
+                    } else {
+                        tokens.add(currentToken.toString());
+                        currentState = State.START;
+
+                    }
+                } case DIGIT -> {
+                    if (Character.isDigit(c)) {
+                        currentToken.append(c);
+                        charQueue.poll();
+                    } else {
+                        tokens.add(currentToken.toString());
+                        currentState = State.START;
+
+                    }
+
+                } case WHITESPACE -> {
+                    if (Character.isWhitespace(c)) {
+                        currentToken.append(c);
+                        charQueue.poll();
+                    } else {
+                        tokens.add(currentToken.toString());
+                        currentState = State.START;
+
+                    }
+
+                } case OPERATOR -> {
+                    if (isOperator(c)) {
+                        currentToken.append(c);
+                        charQueue.poll();
+                    } else {
+                        tokens.add(currentToken.toString());
+                        currentState = State.START;
+
+                    }
+                } case SPECIAL_CHAR -> {
+                    if (!Character.isLetterOrDigit(c) && !Character.isWhitespace(c) && !isOperator(c)) {
+                        currentToken.append(c);
+                        charQueue.poll();
+                    } else {
+                        tokens.add(currentToken.toString());
+                        currentState = State.START;
+
+                    }
+
+                }
+
+            }
+
+        }
         if (!currentToken.isEmpty()) {
             tokens.add(currentToken.toString());
+
         }
 
         return tokens;
+
     }
 
     private static boolean isOperator(char c) {
         return c == '+' || c == '-' || c == '*' || c == '/' || c == '=' || c == ';'
-                || c == '(' || c == ')';
+                || c == '(' || c == ')' || c == '.' || c == '%' || c == '!' || c == '<'
+                || c == '>' || c == '|' || c == '&';
+
     }
 
     public static void main(String[] args) {
-        String inputString = "System.out.println(\"Hello World!\");";
+        String inputString = "System.out.println(\"Hello World!!!\")";
         List<String> tokens = tokenizeString(inputString);
 
         System.out.println("Tokens: " + tokens);
+
     }
 
 }
